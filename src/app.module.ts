1ca24105858c84bd90acb37252312e71c5126eb9
sungalex <cloudallera@gmail.com>
2022-08-20 00:17:11 +0900

    Set logging to true only in production environments

--- src/app.module.ts
+++ src/app.module.ts
@@ -31,7 +31,7 @@ import { Restaurant } from './restaurants/entities/restaurant.entity';
       database: process.env.DB_NAME,
       entities: [Restaurant],
       synchronize: process.env.NODE_ENV !== 'prod',
-      logging: true,
+      logging: process.env.NODE_ENV !== 'prod',
     }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
